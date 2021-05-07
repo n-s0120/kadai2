c20a569e00ae77939caaa43eabe3bed7d3c5ddee
n-s0120 <satoshicuni@gmail.com>
2021-05-07 06:52:37 +0000

    kadai2-0507-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
+  if book.save
+    flash[:notice] = "Book was successfully created"
     redirect_to book_path(book.id)
+  else
+    render action: :new
+  end
   end
 
   def edit
@@ -20,14 +24,22 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+  if book.save
+    flash[:notice] = "Book was successfully updated"
+    redirect_to book_path(book.id)
+  else
+    render action: :new
+  end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
+  if book.destroy
+    flash[:notice] = "Book was successfully destroyed"
     redirect_to books_path
+  else
+    render action: :new
+  end
   end
 
   private
